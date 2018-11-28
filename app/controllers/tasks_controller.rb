fdc3439611a6b97e9513314913b6b344c04822b7
tejas <renpa.inc@gmail.com>
2018-11-28 14:22:11 +0900

    Chapter 7-1 タスク登録の実行前に確認画面を挟む

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,12 @@ class TasksController < ApplicationController
 
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+    
     if @task.save
       redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
     else
@@ -34,6 +40,10 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク「#{@task.name}」は削除しました。"
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
 private
 
   def task_params
