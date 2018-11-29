c23c5b196540a09b83bd7d38e63a3fb420b2cb59
tejas <renpa.inc@gmail.com>
2018-11-29 13:47:59 +0900

    Chapter 7-6 CSV形式のファイルのインポート/エクスポート

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,11 @@ class TasksController < ApplicationController
   def index
     @q = current_user.tasks.ransack(params[:q])
     @tasks = @q.result(distinct: true)
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv"}
+    end
   end
 
   def show
@@ -46,6 +51,12 @@ class TasksController < ApplicationController
     @task = current_user.tasks.new(task_params)
     render :new unless @task.valid?
   end
+
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
+  end
+  
 private
 
   def task_params
