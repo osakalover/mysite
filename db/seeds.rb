ed2c014f40ec6ffc6e1113aaaff49e870f3faf2d
tejas <renpa.inc@gmail.com>
2018-12-01 11:00:11 +0900

    Chapter 9-4-3 初期データ・テスト用データの共有(Seedデータ)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.find_or_create_by!(email: 'admin@example.com') do |user|
+  user.name = 'admin'
+  user.admin = true
+  user.password = 'password'
+  user.password_confirmation = 'password'
+end
+
+User.find_or_create_by!(email: 'user@example.com') do |user|
+  user.name = 'user'
+  user.admin = false
+  user.password = 'password'
+  user.password_confirmation = 'password'
+end
+
+Task.create!(
+  name: 'Task for admin.',
+  description: 'Task created by admin user using seed. Lets see if this works',
+  user_id: User.find_by(name: 'admin').id
+)
+
+Task.create!(
+  name: 'Task for regular user',
+  description: 'Task created by regular user (non-admin) using seed. Hope this works',
+  user_id: User.find_by(name: 'user').id
+)
